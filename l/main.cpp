3ab9752663acf0b708bf6ad01f9367e3d57456fc
lxy2001 <1771478860@qq.com>
2019-06-23 17:48:09 +0800

    啦啦啦
    lll

--- l/main.cpp
+++ l/main.cpp
@@ -1,15 +1,62 @@
-//
-//  main.cpp
-//  l
-//
-//  Created by s20181106279 on 2019/6/23.
-//  Copyright © 2019 s20181106279. All rights reserved.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <string>
+using namespace std;
+void delchar(string str,char c){
+    string s2;
+    int q=0;
+    for(int i=0;str[i]!='\0';i++){
+        q++;
+    }
+    int p=0;
+    for(int j=0;j<q;j++){
+        if(str[j]!=c){
+            s2[p]=str[j];
+            p++;
+        }
+    }
+    s2[p]='\0';
+    cout<<"result: ";
+    for(int k=0;s2[k]!='\0';k++){
+        cout<<s2[k];
+    }
+    cout<<'\n';
+    
+}
+int main (){
+    int a;
+    cin>>a;
+    getchar();
+    string s1;
+    char c;
+    for(int i=1;i<=a;i++){
+        getline(cin,s1);
+        cin>>c;
+        delchar(s1,c);
+        getchar();
+    }
+    
+    
+    
     return 0;
 }
+#include <iostream>
+using namespace std;
+template<class T>
+class Point{
+private:
+    T a,b,c;
+public:
+    Point(T w,T e,T p){
+        a=w;
+        b=e;
+        c=p;
+    }
+    Point(T w,T e){
+        a=w;
+        b=e;
+    }
+    double dist(T a,T b){
+        
+    }
+};
+
